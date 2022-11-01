ffeb613fd653b78fe2b3ea48378343b008e3f0d1
Martin Joukl <jouklma1@gmail.com>
2022-11-01 13:12:31 +0100

    Přidejte soubory projektu.

---
+++ Informacni_System_Pojistovny/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Informacni_System_Pojistovny.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

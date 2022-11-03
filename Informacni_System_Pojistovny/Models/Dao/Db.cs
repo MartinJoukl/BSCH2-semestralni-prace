499690d98dc37b636cf0cba6dde3a16503dc6f2c
JakubJoukl <st64134@upce.cz>
2022-11-03 19:10:59 +0100

    spatna zavislost :C

--- Informacni_System_Pojistovny/Models/Dao/Db.cs
+++ Informacni_System_Pojistovny/Models/Dao/Db.cs
@@ -9,15 +9,17 @@
         {
             string connString = "DATA SOURCE=fei-sql1.upceucebny.cz:1521/IDAS;" +
             "PERSIST SECURITY INFO=True;" +
-            "USER ID=xxx; " +
-            "password=xxx; " +
+            "USER ID=st64134;" +
+            "password=jouklj;" +
             "Pooling = False;" +
-            "Connection Timeout=5000;";
+            "Connection Timeout=5;";
+
+            string connString2 = " Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = fei-sql1.upceucebny.cz)(PORT = 1521))) " +
+                "(CONNECT_DATA = (SERVER = DEDICATED)(SID = IDAS))); User Id = st64134;Password=jouklj;";
             public OracleConnection Connection { get; }
             public Db()
             {
-                OracleConnection conn = new OracleConnection(connString);
-
+                OracleConnection conn = new OracleConnection(connString2);
                 conn.Open();
                 Console.WriteLine(conn.State);
                 conn.Dispose();

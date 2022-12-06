8822fe41ab1193bf4d0b259cb9a6e9a86b789ac2
Martin Joukl <jouklma1@gmail.com>
2022-12-06 19:04:11 +0100

    Fix authentization and impersonification + some random shit

--- Informacni_System_Pojistovny/Models/Model/CustomCookieAuthenticationEvents.cs
+++ Informacni_System_Pojistovny/Models/Model/CustomCookieAuthenticationEvents.cs
@@ -5,6 +5,7 @@
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Authentication.Cookies;
     using Oracle.ManagedDataAccess.Client;
+    using System.Security.Claims;
 
     public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
     {
@@ -19,6 +20,24 @@
         {
             var userPrincipal = context.Principal;
             DateTime casZmeny = DateTime.MinValue; //temp
+            var parameters = new Dictionary<string, object>();
+
+            var originalId = (from c in userPrincipal.Claims
+                              where c.Type == "originalId"
+                              select c.Value).FirstOrDefault();
+            bool impersonification = originalId != null;
+            //if lastChangedOring != null, impersonification is active
+            if (originalId != null)
+            {
+                var lastChangedOring = (from c in userPrincipal.Claims
+                                        where c.Type == "originalCasZmeny"
+                                        select c.Value).FirstOrDefault();
+                parameters.Add("id", originalId);
+                DateTime.TryParse(lastChangedOring, out DateTime dateTimeClaimOrig);
+                bool signedOut = await SignOutIfDateTimeDiffers(context, casZmeny, parameters, dateTimeClaimOrig, false);
+                if (signedOut) { return; }
+                parameters.Remove("id");
+            }
 
             // Look for the LastChanged claim.
             var lastChanged = (from c in userPrincipal.Claims
@@ -28,10 +47,14 @@
             var id = (from c in userPrincipal.Claims
                       where c.Type == "Id"
                       select c.Value).FirstOrDefault();
+            parameters.Add("id", id);
 
-            var parameters = new Dictionary<string, object>();
-            parameters.Add(":id", id);
+            await SignOutIfDateTimeDiffers(context, casZmeny, parameters, dateTimeClaim, impersonification, id);
+        }
 
+        private async Task<bool> SignOutIfDateTimeDiffers(CookieValidatePrincipalContext context, DateTime casZmeny, Dictionary<string, object> parameters, DateTime dateTimeClaim, bool updateImpersonification, string id = null)
+        {
+            //read last time changed
             OracleDataReader reader = _db.ExecuteRetrievingCommand("Select Cas_Zmeny from uzivatele where id = :id", parameters);
             if (reader.Read())
             {
@@ -48,11 +71,71 @@
 
             if (!casZmeny.Equals(dateTimeClaim))
             {
+                if (updateImpersonification && id != null)
+                {
+                    parameters = await ChangeImpersonificationInfo(context, id);
+                }
+                else
+                {
+                    context.RejectPrincipal();
+
+                    await context.HttpContext.SignOutAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<Dictionary<string, object>> ChangeImpersonificationInfo(CookieValidatePrincipalContext context, string id)
+        {
+            Dictionary<string, object> parameters = new();
+            UzivatelModel uzivatelModel = new UzivatelModel(_db);
+            Uzivatel impersonifikovany = uzivatelModel.GetUzivatel(int.Parse(id));
+            var userPrincipal = context.Principal;
+
+            parameters.Add("id", id);
+
+            if (impersonifikovany == null)
+            {
                 context.RejectPrincipal();
 
                 await context.HttpContext.SignOutAsync(
                     CookieAuthenticationDefaults.AuthenticationScheme);
+                throw new Exception("Impersonifikace selhala!");
             }
+
+
+            var claims = new List<Claim>
+                 {
+                 new Claim(ClaimTypes.Email, impersonifikovany.Email),
+                 new Claim(ClaimTypes.Name, impersonifikovany.Jmeno),
+                 new Claim(ClaimTypes.Surname, impersonifikovany.Prijmeni),
+                 new Claim(ClaimTypes.Role, impersonifikovany.Role.ToString()),
+                 new Claim("CasZmeny", impersonifikovany.casZmeny.ToString()),
+                 new Claim("Id", impersonifikovany.Id.ToString())
+                 };
+            string originalMail = userPrincipal.Claims.Where((claim) => claim.Type == "originalMail").First().Value;
+            string originalRole = userPrincipal.Claims.Where((claim) => claim.Type == "originalRole").First().Value;
+            string originalCasZmeny = userPrincipal.Claims.Where((claim) => claim.Type == "originalCasZmeny").First().Value;
+            string originalId = userPrincipal.Claims.Where((claim) => claim.Type == "originalId").First().Value;
+
+            claims.Add(new Claim("originalMail", originalMail));
+            claims.Add(new Claim("originalRole", originalRole));
+            claims.Add(new Claim("originalCasZmeny", originalCasZmeny));
+            claims.Add(new Claim("originalId", originalId));
+
+
+            var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+            await context.HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+            return parameters;
         }
     }
 }

34d43b37ef27e138d8678956dce8f71d1e3ecead
Martin Joukl <jouklma1@gmail.com>
2022-12-18 17:48:30 +0100

    half-commit

---
+++ Informacni_System_Pojistovny/Controllers/PojistnaUdalostController.cs
@@ -0,0 +1,99 @@
+﻿using Informacni_System_Pojistovny.Models.Dao;
+using Informacni_System_Pojistovny.Models.Entity;
+using Informacni_System_Pojistovny.Models.Model;
+using Informacni_System_Pojistovny.Models.Model.PojistnaUdalostModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Informacni_System_Pojistovny.Controllers
+{
+    public class PojistnaUdalostController : Controller
+    {
+        private readonly Db _db;
+        public PojistnaUdalostController(Db db)
+        {
+            _db = db;
+        }
+        // GET: PojistnaUdalostController
+        public ActionResult Index()
+        {
+            PojistnaUdalostModel pojistnaUdalostModel = new PojistnaUdalostModel(_db);
+            List<PojistnaUdalost> pojistneUdalosti = pojistnaUdalostModel.ListPojistnaUdalost();
+            return View(pojistneUdalosti);
+        }
+
+        // GET: PojistnaUdalostController/Details/5
+        public ActionResult Details(int id)
+        {
+            PojistnaUdalostModel pojistnaUdalostModel = new PojistnaUdalostModel(_db);
+            PojistnaUdalost pojistneUdalosti = pojistnaUdalostModel.GetPojistnaUdalost(id);
+            return View(pojistneUdalosti);
+        }
+
+        // GET: PojistnaUdalostController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PojistnaUdalostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PojistnaUdalostController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            PojistnaUdalostModel pojistnaUdalostModel = new PojistnaUdalostModel(_db);
+            PojistnaUdalost pojistneUdalosti = pojistnaUdalostModel.GetPojistnaUdalost(id);
+            KlientModel klientModel = new KlientModel(_db);
+            return View();
+        }
+
+        // POST: PojistnaUdalostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PojistnaUdalostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PojistnaUdalostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Informacni_System_Pojistovny/Models/Entity/PojistnaUdalost.cs
+++ Informacni_System_Pojistovny/Models/Entity/PojistnaUdalost.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
 
 namespace Informacni_System_Pojistovny.Models.Entity
 {
     public class PojistnaUdalost
     {
+        [Display(Name = "Id")]
         public int PojistnaUdalostId { get; set; }
         public Klient Klient { get; set; }
+        [Display(Name = "Vznik pojistky"), DataType(DataType.Date)]
         public DateTime Vznik { get; set; }
+        [Display(Name = "Popis")]
         public string Popis { get; set; }
+        [Display(Name = "Nárokovaná výše pojistky")]
         public int NarokovanaVysePojistky { get; set; }
         public List<Zavazek> Zavazky { get; set; } = new List<Zavazek>();
     }
